de7f100c511010be6d36be5164dda46ec9c7be34
Nithin Shenoy <nithin_shenoy@hotmail.com>
2012-03-06 18:10:04 -0500

    Created initial tools pages

--- uknit/MainPage.xaml.cs
+++ uknit/MainPage.xaml.cs
@@ -12,10 +12,6 @@ namespace uknit
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
-		private SolidColorBrush SteelBlue = new SolidColorBrush(Color.FromArgb(0xFF, 0x46, 0x82, 0xb4));
-		private SolidColorBrush WhiteSmoke = new SolidColorBrush(Color.FromArgb(0xFF, 0xF5, 0xF5, 0xF5));
-		private bool isNew = true;
-
 		// Constructor
 		public MainPage()
 		{
@@ -23,8 +19,6 @@ namespace uknit
 
 			DataContext = App.ViewModel;
 			this.Loaded += new RoutedEventHandler(MainPage_Loaded);
-
-			isNew = true;
 		}
 
 		// Load data for the ViewModel Items
@@ -52,23 +46,11 @@ namespace uknit
 
 		protected override void OnNavigatedFrom(NavigationEventArgs e)
 		{
-			State["Meters.Text"] = Meters.Text;
-			State["Yards.Text"] = Yards.Text;
-
 			base.OnNavigatedFrom(e);
 		}
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
-			//BitmapImage backgroundImage = ConfigurationModel.GetBackgroundImage();
-
-			//if(backgroundImage != null)
-			//{
-			//    ImageBrush img = new ImageBrush();
-			//    img.ImageSource = backgroundImage;
-			//    this.MainPagePanorama.Background = img;
-			//}
-
 			IDictionary<string, string> queryString = this.NavigationContext.QueryString;
 
 			if(queryString.ContainsKey("AddEditProject"))
@@ -76,23 +58,6 @@ namespace uknit
 				this.NavigationService.RemoveBackEntry();
 				this.NavigationService.RemoveBackEntry();
 			}
-
-			if(isNew)
-			{
-				object val;
-
-				if(State.TryGetValue("Meters.Text", out val))
-				{
-					Meters.Text = val as string;
-				}
-
-				if(State.TryGetValue("Yards.Text", out val))
-				{
-					Yards.Text = val as string;
-				}
-			}
-
-			isNew = false;
 
 			base.OnNavigatedTo(e);
 		}
@@ -109,50 +74,6 @@ namespace uknit
 			NavigationService.Navigate(new Uri(rowCounterPageUri, UriKind.Relative));
 		}
 
-		private void Convert_Click(object sender, RoutedEventArgs e)
-		{
-			// No need to do anything since the LostFocus handlers do the job.
-		}
-
-		private void Yards_GotFocus(object sender, RoutedEventArgs e)
-		{
-			MetersLabel.Foreground = WhiteSmoke;
-			YardsLabel.Foreground = SteelBlue;
-		}
-
-		private void Yards_LostFocus(object sender, RoutedEventArgs e)
-		{
-			// yards * 0.9144
-			double meters;
-			double yards;
-
-			if(double.TryParse(Yards.Text, out yards))
-			{
-				meters = Math.Round(yards * 0.9144, 2);
-				Meters.Text = meters.ToString();
-			}
-		}
-
-		private void Meters_GotFocus(object sender, RoutedEventArgs e)
-		{
-			YardsLabel.Foreground = WhiteSmoke;
-			MetersLabel.Foreground = SteelBlue;
-
-		}
-
-		private void Meters_LostFocus(object sender, RoutedEventArgs e)
-		{
-			// meters * 1.0936133
-			double meters;
-			double yards;
-
-			if(double.TryParse(Meters.Text, out meters))
-			{
-				yards = Math.Round(meters * 1.0936133, 2);
-				Yards.Text = yards.ToString();
-			}
-
-		}
 
 		private void OnContextMenuClick_EditProject(object sender, RoutedEventArgs e)
 		{
@@ -194,6 +115,16 @@ namespace uknit
 			int index = App.ViewModel.KnittingProjects.IndexOf(project);
 			App.ViewModel.KnittingProjects.RemoveAt(index);
 			ConfigurationModel.RemoveKnittingProjectByName(projectName);
+		}
+
+		private void OnClick_Tools_YardsMeters(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Views/Tools_YardsMeters.xaml", UriKind.Relative));
+		}
+
+		private void OnClick_Tools_Gauge(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Views/Tools_GaugeCalculator.xaml", UriKind.Relative));
 		}
 	}
 }
