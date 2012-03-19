beb647b402ec0705cdb104465008b44f36022a52
Nithin Shenoy <nithin_shenoy@hotmail.com>
2012-03-18 23:41:15 -0400

    Added Gauge Calculator

--- uknit/Views/Tools/GaugeCalculator.xaml.cs
+++ uknit/Views/Tools/GaugeCalculator.xaml.cs
@@ -10,14 +10,73 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using uknit.ViewModels;
 
 namespace uknit.Views
 {
 	public partial class GaugeCalculator : PhoneApplicationPage
 	{
+		private GaugeViewModel viewModel = new GaugeViewModel();
+
 		public GaugeCalculator()
 		{
 			InitializeComponent();
+
+			this.DataContext = viewModel;
+		}
+
+		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			if(viewModel.DetectSettingsChange())
+			{
+				this.MeasuredLength.SelectedIndex = 0;
+				viewModel.Reinitialize();
+			}
+			base.OnNavigatedTo(e);
+		}
+
+		private void Measurement_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			CalculateGauge();
+		}
+
+		private void Measurement_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			CalculateGauge();
+		}
+
+		private void OnClick_Settings(object sender, EventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Views/Settings.xaml", UriKind.Relative));
+		}
+
+		private void CalculateGauge()
+		{
+			int patternGauge = 0;
+			int measuredStitches = 0;
+			int measuredLength = 0;
+
+			if(!String.IsNullOrWhiteSpace(this.PatternGauge.Text))
+			{
+				patternGauge = int.Parse(this.PatternGauge.Text);
+			}
+
+			if(!String.IsNullOrWhiteSpace(this.MeasuredStitches.Text))
+			{
+				measuredStitches = int.Parse(this.MeasuredStitches.Text);
+			}
+
+			string measuredLengthText = this.MeasuredLength.Items[this.MeasuredLength.SelectedIndex] as string;
+			if(!String.IsNullOrWhiteSpace(measuredLengthText))
+			{
+				measuredLength = int.Parse(measuredLengthText.Substring(0, measuredLengthText.Length - 3));
+			}
+
+			if((patternGauge > 0) && (measuredStitches > 0) && (measuredLength > 0))
+			{
+				viewModel.CalculateActualGauge(measuredStitches, measuredLength);
+				viewModel.MeasureGaugeGuidance(patternGauge);
+			}
 		}
 	}
 }
